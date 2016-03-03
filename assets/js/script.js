c04269da9bc450e54200ab0d612291127afd862e
derek.morash <derek.morash@gmail.com>
2016-03-03 09:15:17 -0400

    Working on breaking up the tweets data

--- assets/js/script.js
+++ assets/js/script.js
@@ -144,51 +144,68 @@ myApp.controller('RegistrationController',
 }]); //contoller
 
 myApp.controller('TweetController',
-  ['$scope', '$rootScope', '$firebaseAuth', '$firebaseArray', 'FIREBASE_URL',
-  function($scope, $rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL) {
+['$scope', '$rootScope', '$firebaseAuth', '$firebaseArray', 'FIREBASE_URL',
+function($scope, $rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL) {
 
-    var ref = new Firebase(FIREBASE_URL);
-    var auth = $firebaseAuth(ref);
+  var ref = new Firebase(FIREBASE_URL);
+  var auth = $firebaseAuth(ref);
 
 
-    auth.$onAuth(function(authUser) {
-      if(authUser) {
-        //create a reference to where the tweets will be stored
-        var tweetsRef = new Firebase(FIREBASE_URL + 'tweets/');
-        var tweetInfo = $firebaseArray(tweetsRef);
+  auth.$onAuth(function(authUser) {
+    if(authUser) {
+      //create a reference to where the tweets will be stored
+      var tweetsRef = new Firebase(FIREBASE_URL + 'tweets/');
+      var usersRef = new Firebase(FIREBASE_URL + 'users/');
+      var tweetInfo = $firebaseArray(tweetsRef);
+      var usersInfo = $firebaseArray(usersRef);
 
-        $scope.tweets = tweetInfo;
-        console.log($scope.tweets);
+      console.log(tweetInfo);
 
-        //when the addTweet form is submitted
-        $scope.addTweet = function() {
-          //gather the tweet info
-          tweetInfo.$add({
-            userID: $rootScope.currentUser.$id,
-            tweet: $scope.tweet,
-            date: Firebase.ServerValue.TIMESTAMP
-          }).then(function() { //promise from firebase when the tweet is added
-            $scope.tweet = '';
-          }); //promise
-        }; //addTweet
+      $scope.tweets = [];
 
-        $scope.deleteTweet = function(key) {
+      tweetsRef.on("value", function(snapshot) {
+        snapshot.forEach(function(child) {
+          console.log(child.val());
+          $scope.tweets.push(child.val());
+        });
+      });
+
+      // $scope.tweets = tweetInfo;
+      $scope.users = usersInfo;
+
+      //when the addTweet form is submitted
+      $scope.addTweet = function() {
+        //gather the tweet info
+        tweetInfo.$add({
+          userID: $rootScope.currentUser.$id,
+          tweet: $scope.tweet,
+          date: Firebase.ServerValue.TIMESTAMP
+        }).then(function() { //promise from firebase when the tweet is added
+          $scope.tweet = '';
+        }); //promise
+      }; //addTweet
+
+      //delteing tweets
+      $scope.deleteTweet = function(key) {
+        //only delete a tweet if the tweet belongs to the logged in user
+        if($rootScope.currentUser.$id === tweetInfo[key].userID) {
           tweetInfo.$remove(key);
-        };
-      } //if there is an authenticated user
-    }); //onAuth
-  }]) //FeedController
+        }
+      };
+    } //if there is an authenticated user
+  }); //onAuth
+}]) //FeedController
 
 .directive('tweet', function() {
   return {
-    // restrict: 'E',
-    templateUrl: 'views/tweet.html'
-  };
-});
+    restrict: 'E',
+    templateUrl: 'views/tweet.html',
+    controller: function() {
+
+    } //controller
+  }; //return
+}); //directive
 
 
 // get all tweets
 // if the tweet belongs to current user, add edit and delete buttons. Use onAuth from the authentication factory service
-
-
-//DELETE button
